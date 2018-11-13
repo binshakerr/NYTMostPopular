27f5a4f82d6e8b12234c64294fdeb55569243b86
eslam shaker <binshakerr@gmail.com>
2018-11-13 10:49:42 +0200

    master screen is finished

--- NYTMostPopular/NYTMostPopular/View/MasterCell.swift
+++ NYTMostPopular/NYTMostPopular/View/MasterCell.swift
@@ -7,20 +7,35 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MasterCell: UITableViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var NewsImageView: UIImageView!
+    
+    
     var result: Result! {
         didSet {
+            titleLabel.text = result.title
+            detailsLabel.text = result.abstract
+            dateLabel.text = result.publishedDate
             
+            var imageUrlString = ""
+            for media in result.media {
+                for metadata in media.mediaMetadata {
+                    if metadata.height == 320 {
+                        imageUrlString = metadata.url
+                    }
+                }
+            }
+            
+            NewsImageView.sd_setShowActivityIndicatorView(true)
+            NewsImageView.sd_setIndicatorStyle(.gray)
+            NewsImageView.sd_setImage(with: URL(string: imageUrlString))
         }
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-    }
-
-    
 
 }
